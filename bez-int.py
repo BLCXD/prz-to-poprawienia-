015e246d0a9ce2b412779d4697e2665b0c47d239
BLCXD <95041805+BLCXD@users.noreply.github.com>
2024-11-28 15:51:43 +0100

    Create bez-int.py

---
+++ bez-int.py
@@ -0,0 +1,57 @@
+# Tutaj pisz swój kod, młody padawanie ;-)
+def rozklad(n):
+    i = 2
+    czyn = []
+
+    while i*i <= n:
+        while n % i == 0:
+            czyn.append(i)
+            n //= i
+        i+=1
+
+    if n > 1:
+        czyn.append(n)
+    return sum(czyn) - nwd
+
+
+
+
+
+def fsmult(b):
+    return rozklad(b)
+
+def fnwd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+a, b = map(int, input().split())
+
+if a == b:
+    print(0)
+
+else:
+    nwd = fnwd(a, b)
+    nww = (a * b) // nwd
+
+    if b > a:
+        if b % a != 0:
+            x = a // nwd
+            c = a // x
+            y = b // c
+            if c * y == b:
+                smult = fsmult(b)
+                print(x + smult)
+
+        elif b%a == 0:
+            print(b // a)
+    else:
+        if a%b ==0:
+            print(a//b)
+        else:
+
+            x = nww // a
+            y = nww // b
+            print(x + y)
+
+#print(fnwd(a,b))
